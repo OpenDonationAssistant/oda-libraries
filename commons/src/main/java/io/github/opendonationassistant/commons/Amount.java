99702df61d3a0ff89a154b22113229d71c10daa0
stCarolas <stcarolas@gmail.com>
2024-07-17 17:10:41 +0300

    add equals and hascode to Amount

--- commons/src/main/java/io/github/opendonationassistant/commons/Amount.java
+++ commons/src/main/java/io/github/opendonationassistant/commons/Amount.java
@@ -42,4 +42,42 @@ public class Amount {
       "]"
     );
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((major == null) ? 0 : major.hashCode());
+    result = prime * result + ((minor == null) ? 0 : minor.hashCode());
+    result = prime * result + ((currency == null) ? 0 : currency.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Amount other = (Amount) obj;
+    if (major == null) {
+      if (other.major != null)
+        return false;
+    } else if (!major.equals(other.major))
+      return false;
+    if (minor == null) {
+      if (other.minor != null)
+        return false;
+    } else if (!minor.equals(other.minor))
+      return false;
+    if (currency == null) {
+      if (other.currency != null)
+        return false;
+    } else if (!currency.equals(other.currency))
+      return false;
+    return true;
+  }
+
 }
