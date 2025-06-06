c13766338407ba8d84a1685d098bd5b77ee427a6
stCarolas <stcarolas@gmail.com>
2025-06-06 23:17:50 +0300

    add sending raw string to config changes queue

--- rabbit-conf/src/main/java/io/github/opendonationassistant/events/config/ConfigCommandSender.java
+++ rabbit-conf/src/main/java/io/github/opendonationassistant/events/config/ConfigCommandSender.java
@@ -3,6 +3,8 @@ package io.github.opendonationassistant.events.config;
 import io.github.opendonationassistant.commons.logging.ODALogger;
 import io.micronaut.rabbitmq.annotation.Binding;
 import io.micronaut.rabbitmq.annotation.RabbitClient;
+import io.micronaut.serde.ObjectMapper;
+import java.io.IOException;
 import java.util.Map;
 
 @RabbitClient("commands")
@@ -11,10 +13,17 @@ public interface ConfigCommandSender {
 
   void send(@Binding String binding, ConfigPutCommand command);
 
+  void sendSerialized(@Binding String binding, String command);
+
   default void send(ConfigPutCommand command) {
     log.context(Map.of("command", command));
-    send("config", command);
+    var mapper = ObjectMapper.getDefault();
+    try {
+      sendSerialized("config", mapper.writeValueAsString(command));
+    } catch (IOException e) {
+      log.info("Can't serialize command", Map.of("command", command));
+      e.printStackTrace();
+    }
     log.clear();
   }
-
 }
