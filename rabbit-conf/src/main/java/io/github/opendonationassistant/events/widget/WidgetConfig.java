7d1caedbf4696d59a53b8107309badd245c41559
stCarolas <stcarolas@gmail.com>
2025-06-12 17:47:05 +0300

    add getting property from config

--- rabbit-conf/src/main/java/io/github/opendonationassistant/events/widget/WidgetConfig.java
+++ rabbit-conf/src/main/java/io/github/opendonationassistant/events/widget/WidgetConfig.java
@@ -1,11 +1,24 @@
 package io.github.opendonationassistant.events.widget;
 
 import io.github.opendonationassistant.commons.ToString;
+import io.micronaut.core.util.StringUtils;
 import io.micronaut.serde.annotation.Serdeable;
+import jakarta.annotation.Nonnull;
 import java.util.List;
+import java.util.Optional;
 
 @Serdeable
-public record WidgetConfig(List<WidgetProperty> properties) {
+public record WidgetConfig(@Nonnull List<WidgetProperty> properties) {
+  public @Nonnull Optional<WidgetProperty> get(String name) {
+    if (StringUtils.isEmpty(name)) {
+      return Optional.empty();
+    }
+    return properties()
+      .stream()
+      .filter(prop -> name.equals(prop.name()))
+      .findFirst();
+  }
+
   @Override
   public String toString() {
     return ToString.asJson(this);
