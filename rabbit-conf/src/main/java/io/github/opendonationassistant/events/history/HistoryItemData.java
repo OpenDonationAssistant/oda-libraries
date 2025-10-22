abe830f1ac69be860d2095ffb81d5def7c0de36b
stCarolas <stcarolas@gmail.com>
2025-10-23 01:25:46 +0300

    add AlertMedia to HistoryItemData

--- rabbit-conf/src/main/java/io/github/opendonationassistant/events/history/HistoryItemData.java
+++ rabbit-conf/src/main/java/io/github/opendonationassistant/events/history/HistoryItemData.java
@@ -5,6 +5,8 @@ import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedProperty;
 import io.micronaut.data.model.DataType;
 import io.micronaut.serde.annotation.Serdeable;
+import jakarta.annotation.Nullable;
+
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
@@ -25,8 +27,11 @@ public record HistoryItemData(
   @MappedProperty(type = DataType.JSON) List<Attachment> attachments,
   @MappedProperty(type = DataType.JSON) List<TargetGoal> goals,
   @MappedProperty(type = DataType.JSON) List<ReelResult> reelResults,
-  @MappedProperty(type = DataType.JSON) List<ActionRequest> actions
+  @MappedProperty(type = DataType.JSON) List<ActionRequest> actions,
+  @Nullable @MappedProperty(type = DataType.JSON) AlertMedia media
 ) {
   @Serdeable
   public record ActionRequest(String id, String actionId, Map<String, Object> payload){}
+  @Serdeable
+  public record AlertMedia(String url){}
 }
