727d97535f9d5fa16af53f01ee462a1b70b4148e
stCarolas <stcarolas@gmail.com>
2026-02-04 14:25:31 +0300

    some improvements for common message handling

--- rabbit-conf/src/main/java/io/github/opendonationassistant/events/MessageHandler.java
+++ rabbit-conf/src/main/java/io/github/opendonationassistant/events/MessageHandler.java
@@ -1,6 +1,8 @@
 package io.github.opendonationassistant.events;
 
+import java.io.IOException;
+
 public interface MessageHandler {
   String type();
-  void handle(byte[] message);
+  void handle(byte[] message) throws IOException;
 }
--- rabbit-conf/src/main/java/io/github/opendonationassistant/events/MessageProcessor.java
+++ rabbit-conf/src/main/java/io/github/opendonationassistant/events/MessageProcessor.java
@@ -1,12 +1,16 @@
 package io.github.opendonationassistant.events;
 
+import io.github.opendonationassistant.commons.logging.ODALogger;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @Singleton
 public class MessageProcessor {
 
+  private final ODALogger log = new ODALogger(this);
   private List<MessageHandler> handlers;
 
   @Inject
@@ -19,6 +23,15 @@ public class MessageProcessor {
       .stream()
       .filter(handler -> handler.type().equals(type))
       .findFirst()
-      .ifPresent(handler -> handler.handle(message));
+      .ifPresent(handler -> {
+        try {
+          handler.handle(message);
+        } catch (IOException e) {
+          log.error(
+            "Error parsing TwitchChannelRaidEvent",
+            Map.of("error", e.getLocalizedMessage())
+          );
+        }
+      });
   }
 }
