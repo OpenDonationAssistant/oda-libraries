696c04bcb9a28a0ce52488cffd5e205c786cb785
stCarolas <stcarolas@gmail.com>
2026-03-08 19:15:52 +0300

    add nullable for basecontroler

--- commons/src/main/java/io/github/opendonationassistant/commons/micronaut/BaseController.java
+++ commons/src/main/java/io/github/opendonationassistant/commons/micronaut/BaseController.java
@@ -3,9 +3,11 @@ package io.github.opendonationassistant.commons.micronaut;
 import io.micronaut.security.authentication.Authentication;
 import java.util.Optional;
 
+import org.jspecify.annotations.Nullable;
+
 public abstract class BaseController {
 
-  protected Optional<String> getOwnerId(Authentication auth) {
+  protected Optional<String> getOwnerId(@Nullable Authentication auth) {
     return Optional.ofNullable(auth)
       .map(Authentication::getAttributes)
       .map(it -> it.get("preferred_username"))
