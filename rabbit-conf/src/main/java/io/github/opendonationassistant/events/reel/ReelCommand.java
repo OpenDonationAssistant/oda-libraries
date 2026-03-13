bb207efe69854d9853252133023ad7c39db287c2
stCarolas <stcarolas@gmail.com>
2026-03-13 14:58:34 +0300

    add source and originId to TriggerReelCommand

--- rabbit-conf/src/main/java/io/github/opendonationassistant/events/reel/ReelCommand.java
+++ rabbit-conf/src/main/java/io/github/opendonationassistant/events/reel/ReelCommand.java
@@ -1,12 +1,15 @@
 package io.github.opendonationassistant.events.reel;
 
 import io.micronaut.serde.annotation.Serdeable;
+import org.jspecify.annotations.Nullable;
 
 public class ReelCommand {
 
   @Serdeable
   public static record TriggerReelCommand(
     String widgetId,
-    String recipientId
+    String recipientId,
+    String source,
+    @Nullable String originId
   ) {}
 }
