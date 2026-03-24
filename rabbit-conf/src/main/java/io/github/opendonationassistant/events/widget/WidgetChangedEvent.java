27f1b7307947176be9dc68a01ac191c107c79a47
stCarolas <stcarolas@gmail.com>
2026-03-24 21:33:00 +0300

    add source and originId to WidgetChangedEvent

--- rabbit-conf/src/main/java/io/github/opendonationassistant/events/widget/WidgetChangedEvent.java
+++ rabbit-conf/src/main/java/io/github/opendonationassistant/events/widget/WidgetChangedEvent.java
@@ -1,6 +1,13 @@
 package io.github.opendonationassistant.events.widget;
 
+import org.jspecify.annotations.Nullable;
+
 import io.micronaut.serde.annotation.Serdeable;
 
 @Serdeable
-public record WidgetChangedEvent(String type, Widget widget) {}
+public record WidgetChangedEvent(
+  String type,
+  Widget widget,
+  String source,
+  @Nullable String originId
+) {}
