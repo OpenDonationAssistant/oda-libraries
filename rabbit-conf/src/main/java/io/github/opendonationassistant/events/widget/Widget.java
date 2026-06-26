a5240af0e37912e3e4d43fae5f4f665ae1173817
stCarolas <stcarolas@gmail.com>
2026-06-26 18:58:58 +0300

    refack widget events, add 'deleted' flag

--- rabbit-conf/src/main/java/io/github/opendonationassistant/events/widget/Widget.java
+++ rabbit-conf/src/main/java/io/github/opendonationassistant/events/widget/Widget.java
@@ -1,7 +1,10 @@
 package io.github.opendonationassistant.events.widget;
 
-import io.github.opendonationassistant.commons.ToString;
+import io.micronaut.core.util.StringUtils;
 import io.micronaut.serde.annotation.Serdeable;
+import java.util.List;
+import java.util.Optional;
+import org.jspecify.annotations.Nullable;
 
 @Serdeable
 public record Widget(
@@ -11,10 +14,32 @@ public record Widget(
   String name,
   Boolean enabled,
   String ownerId,
+  Boolean deleted,
   WidgetConfig config
 ) {
-  @Override
-  public String toString() {
-    return ToString.asJson(this);
+
+  @Serdeable
+  public record WidgetConfig(List<WidgetProperty> properties) {
+    public Optional<WidgetProperty> getProperty(String name) {
+      if (StringUtils.isEmpty(name)) {
+        return Optional.empty();
+      }
+      return properties()
+        .stream()
+        .filter(prop -> name.equals(prop.name()))
+        .findFirst();
+    }
+
+    public <T> Optional<T> getValue(String name) {
+      return this.getProperty(name).map(it -> (T) it);
+    }
   }
+
+  @Serdeable
+  public record WidgetProperty(
+    String name,
+    String displayName,
+    String type,
+    @Nullable Object value
+  ) {}
 }
--- rabbit-conf/src/main/java/io/github/opendonationassistant/events/widget/WidgetCommandSender.java
+++ rabbit-conf/src/main/java/io/github/opendonationassistant/events/widget/WidgetCommandSender.java
@@ -1,20 +1,25 @@
 package io.github.opendonationassistant.events.widget;
 
 import io.github.opendonationassistant.commons.logging.ODALogger;
+import io.github.opendonationassistant.events.widget.Widget.WidgetConfig;
 import io.github.opendonationassistant.rabbit.Exchange;
 import io.github.opendonationassistant.rabbit.Key;
 import io.micronaut.rabbitmq.annotation.Binding;
 import io.micronaut.rabbitmq.annotation.RabbitClient;
+import io.micronaut.serde.annotation.Serdeable;
 import java.util.Map;
 
 @RabbitClient(Exchange.WIDGETS)
 public interface WidgetCommandSender {
   final ODALogger log = new ODALogger(WidgetCommandSender.class);
 
-  void send(@Binding String binding, WidgetUpdateCommand command);
+  @Serdeable
+  public record WidgetUpdateCommand(String id, WidgetConfig patch) {}
 
   default void send(WidgetUpdateCommand command) {
     log.info("Send WidgetUpdateCommand", Map.of("command", command));
     send(Key.COMMAND, command);
   }
+
+  void send(@Binding String binding, WidgetUpdateCommand command);
 }
